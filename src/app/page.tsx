f2d844aefe5ab035031957d547b5ee62aa315abc
khushighimire <khushighimire1127@gmail.com>
2025-01-10 23:15:56 +0545

    added heading2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
        
        <h1>Hello World</h1>
+       <h2>this is heading2</h2>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
        
